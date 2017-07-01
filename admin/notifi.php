6dfed0933bea9d7042cd5fd2f6a887f2ba1b6d52
Thumimku <thumilan.15@cse.mrt.ac.lk>
2017-07-01 17:05:21 +0530

    Admin repo...User interface development developing

---
+++ admin/notifi.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>WESTART VENTURES...ADMIN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Pooled Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--<script src="https://www.w3schools.com/lib/w3.js"></script>-->
+    <script src="js/w3.js"></script>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+
+    <link rel="stylesheet" href="css/morris.css" type="text/css"/>
+    <!-- Graph CSS -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <!-- //jQuery -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+    <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- lined-icons -->
+    <link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/w3.js"></script>
+</head>
+<body>
+<div class="header-main">
+    <div class="logo-w3-agile">
+        <h1><a href="index.php">WestArt Ventures</a></h1>
+    </div>
+    <div class="w3layouts-left">
+
+        <!--search-box-->
+        <div class="w3-search-box">
+            <form action="#" method="post">
+                <input type="text" placeholder="Search..." required="">
+                <input type="submit" value="">
+            </form>
+        </div><!--//end-search-box-->
+        <div class="clearfix"> </div>
+    </div>
+    <div class="w3layouts-right">
+        <div class="profile_details_left"><!--notifications of menu start -->
+            <ul class="nofitications-dropdown">
+                <li class="dropdown head-dpdn">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-cog"></i><span class="badge">3</span></a><!--This span is for number of notification-->
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="notification_header">
+                                <h3>You have 3 new messages</h3>
+                            </div>
+                        </li>
+                        <!--add new customer-->
+                        <li><a href="newcutomermodal.php">
+                            <button type="button" class="btn btn-hover btn-order " >
+
+                                <div class="notification_desc" >
+
+                                    <p>New Shop name/Area</p>
+
+                                    <p><span>1 hour ago</span></p>
+                                </div></button>
+                            <div class="clearfix"></div>
+                        </a></li>
+
+                        <!--Stock empty details and expiry stock details-->
+                        <li><a href="newcutomermodal.php">
+                            <button type="button" class="btn btn-hover btn-order " >
+
+                                <div class="notification_desc" >
+
+                                    <p>Product Name/Problem</p>
+
+
+                                </div></button>
+                            <div class="clearfix"></div>
+                        </a></li>
+
+
+                        <!--return details-->
+                        <li><a href="returnordermodal.php">
+                        <button type="button" class="btn btn-hover btn-order " >
+
+                            <div class="notification_desc" >
+
+                                <p>Order ID/Shop name</p>
+
+                                <p><span>1 hour ago</span></p>
+                            </div></button>
+                        <div class="clearfix"></div>
+                    </a></li>
+
+                        <li>
+                            <div class="notification_bottom">
+                                <a href="#">See all messages</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown head-dpdn">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-cart-arrow-down"></i><span class="badge blue">3</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="notification_header">
+                                <h3>You have 3 new notification </h3>
+                            </div>
+                        </li>
+
+                        <li><a href="ordermodal.php">
+                            <button type="button" class="btn btn-hover btn-order " >
+
+                           <div class="notification_desc" >
+
+                               <p>Order ID/Shop name</p>
+
+                                   <p><span>1 hour ago</span></p>
+                            </div></button>
+                          <div class="clearfix"></div>
+                        </a></li>
+
+                        <!-- Modal -->
+
+                        <!--
+                         <li class="odd"><a href="#">
+                            <div class="user_img"><img src="images/in6.jpg" alt=""></div>
+                           <div class="notification_desc">
+                            <p>Lorem ipsum dolor</p>
+                            <p><span>1 hour ago</span></p>
+                            </div>
+                           <div class="clearfix"></div>
+                         </a></li>
+                         <li><a href="#">
+                            <div class="user_img"><img src="images/in7.jpg" alt=""></div>
+                           <div class="notification_desc">
+                            <p>Lorem ipsum dolor</p>
+                            <p><span>1 hour ago</span></p>
+                            </div>
+                           <div class="clearfix"></div>
+                         </a></li><-->
+                        <li>
+                            <div class="notification_bottom">
+                                <a href="#">See all notifications</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown head-dpdn">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-truck"></i><span class="badge blue1">9</span></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="notification_header">
+                                <h3>You have 1 new Deliver Report</h3>
+                            </div>
+                        </li>
+
+                        <li><a href="deliverymodal.php">
+                            <button type="button" class="btn btn-hover btn-delivery " >
+
+                                <div class="notification_desc" >
+
+                                    <p>Order ID/Shop name</p>
+
+                                    <p><span>1 hour ago</span></p>
+                                </div></button>
+                            <div class="clearfix"></div>
+                        </a></li>
+                        <!--
+                        <li><a href="#">
+                            <div class="task-info">
+                                <span class="task-desc">Dashboard done</span><span class="percentage">90%</span>
+                               <div class="clearfix"></div>
+                            </div>
+                            <div class="progress progress-striped active">
+                                 <div class="bar green" style="width:90%;"></div>
+                            </div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="task-info">
+                                <span class="task-desc">Mobile App</span><span class="percentage">33%</span>
+                                <div class="clearfix"></div>
+                            </div>
+                           <div class="progress progress-striped active">
+                                 <div class="bar red" style="width: 33%;"></div>
+                            </div>
+                        </a></li>
+                        <li><a href="#">
+                            <div class="task-info">
+                                <span class="task-desc">Issues fixed</span><span class="percentage">80%</span>
+                               <div class="clearfix"></div>
+                            </div>
+                            <div class="progress progress-striped active">
+                                 <div class="bar  blue" style="width: 80%;"></div>
+                            </div>
+                        </a></li><-->
+                        <li>
+                            <div class="notification_bottom">
+                                <a href="#">See all pending tasks</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <div class="clearfix"> </div>
+            </ul>
+            <div class="clearfix"> </div>
+        </div>
+        <!--notification menu end -->
+
+        <div class="clearfix"> </div>
+    </div>
+    <div class="profile_details w3l">
+        <ul>
+            <li class="dropdown profile_details_drop">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <div class="profile_img">
+                        <span class="prfil-img"><img src="images/in4.jpg" alt=""> </span>
+                        <div class="user-name">
+                            <p>Admin</p>
+                            <span>Administrator</span>
+                        </div>
+                        <i class="fa fa-angle-down"></i>
+                        <i class="fa fa-angle-up"></i>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+                <ul class="dropdown-menu drp-mnu">
+
+                    <li> <a href="#"><i class="fa fa-user"></i> Profile</a> </li>
+                    <li> <a href="#"><i class="fa fa-sign-out"></i> Logout</a> </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
+    <div class="clearfix"> </div>
+</div>
+
+</body>
+</html>
