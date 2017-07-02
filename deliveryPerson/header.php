7504c9cd7051511bfc192add78fe1ce5528ee566
Sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2017-07-02 13:00:49 +0530

    Header components seperated

---
+++ deliveryPerson/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="page-container">
+<!--/content-inner-->
+<div class="left-content">
+    <div class="mother-grid-inner">
+        <!--header start here-->
+        <div class="header-main">
+            <div class="logo-w3-agile">
+                <h1><a href="index.php">Pooled</a></h1>
+            </div>
+
+        <div class="profile_details w3l">
+                <ul>
+                    <li class="dropdown profile_details_drop">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                            <div class="profile_img">
+                                <span class="prfil-img"><img src="images/in4.jpg" alt=""> </span>
+                                <div class="user-name">
+                                    <p>Malorum</p>
+                                    <span>Administrator</span>
+                                </div>
+                                <i class="fa fa-angle-down"></i>
+                                <i class="fa fa-angle-up"></i>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                        <ul class="dropdown-menu drp-mnu">
+                            <li> <a href="#"><i class="fa fa-cog"></i> Settings</a> </li>
+                            <li> <a href="#"><i class="fa fa-user"></i> Profile</a> </li>
+                            <li> <a href="#"><i class="fa fa-sign-out"></i> Logout</a> </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"> </div>
+        </div>
+
+
+</body>
+</html>
